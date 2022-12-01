2bcda3db6786dab59c9b9f62ff0a7b53cf069a86
alexandre-fernandez <alexandre-fernandez@gmx.com>
2022-12-01 23:27:13 +0100

    initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,30 @@
+module.exports = {
+	parser: "@typescript-eslint/parser",
+	extends: ["af-javascript", "plugin:@typescript-eslint/recommended"],
+	plugins: ["@typescript-eslint"],
+	parserOptions: {
+		sourceType: "module",
+		ecmaVersion: 2020,
+		project: ["tsconfig.json"],
+	},
+	settings: {
+		"import/parsers": {
+			"@typescript-eslint/parser": [".ts", ".tsx"],
+		},
+		"import/resolver": {
+			typescript: {
+				alwaysTryTypes: true,
+				project: ["tsconfig.json"],
+			},
+		},
+	},
+	rules: {
+		"no-unused-vars": "off",
+		"@typescript-eslint/no-unused-vars": "error",
+		"@typescript-eslint/interface-name-prefix": "off",
+		"@typescript-eslint/explicit-function-return-type": "off",
+		"@typescript-eslint/explicit-module-boundary-types": "off",
+		"@typescript-eslint/no-explicit-any": "off",
+		"@typescript-eslint/no-non-null-assertion": "off",
+	},
+}
