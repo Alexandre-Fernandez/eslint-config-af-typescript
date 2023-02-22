09ef7da4964d666b9c79e6435612cee49d2a01a9
alexandre-fernandez <alexandre-fernandez@gmx.com>
2023-02-22 21:21:47 +0100

    @typescript-eslint/ban-ts-comment

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -28,6 +28,7 @@ module.exports = {
 		"@typescript-eslint/explicit-module-boundary-types": "off",
 		"@typescript-eslint/no-explicit-any": "off",
 		"@typescript-eslint/no-non-null-assertion": "off",
+		"@typescript-eslint/ban-ts-comment": "off",
 		"@typescript-eslint/ban-types": [
 			"error",
 			{ types: { Function: false } },
